773156ec51ab3f3304bd35eadda9994620b6f1bb
Phillip Williams <fillip1984@gmail.com>
2024-01-14 12:51:14 -0500

    needed to make items array optional for when you create a brand new list

--- src/types.ts
+++ src/types.ts
@@ -12,7 +12,7 @@ export const listFormSchema = z.object({
   id: z.string().nullish(),
   name: z.string().min(2),
   description: z.string().min(2),
-  items: z.array(itemFormSchema),
+  items: z.array(itemFormSchema).nullish(),
 });
 
 export type ListType = z.infer<typeof listFormSchema>;
