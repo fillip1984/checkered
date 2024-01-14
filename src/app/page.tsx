1882d6873686b7c68548b9d53ff011361fb2e468
Phillip Williams <fillip1984@gmail.com>
2024-01-14 13:27:02 -0500

    creating detail vs edit views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
 
   return (
     <main className="container m-auto">
-      <div className="my-2 flex gap-2">
+      <div className="my-2 flex flex-col gap-2 p-2 md:flex-row">
         {listQuery.data?.map((list) => <ListCard key={list.id} list={list} />)}
         <Link
-          href="/lists/new"
-          className="flex min-h-[130px] min-w-[150px] flex-col items-center justify-center gap-3 rounded border border-accent p-1">
-          New List
+          href="/lists/new/edit"
+          className="flex min-h-[130px] w-full flex-col items-center justify-center gap-3 rounded border border-accent p-2">
+          <span className="text-xl">New List</span>
           <BsPlusLg className="text-4xl" />
         </Link>
       </div>
